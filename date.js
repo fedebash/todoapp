7f2c9b6b6ffd976fac93da9bb3d3d3e4a6d5ac63
tonet <tonet@info.com>
2021-10-26 22:48:39 -0400

    first commit

---
+++ date.js
@@ -0,0 +1,10 @@
+//<![CDATA[
+var date = new Date();
+var d  = date.getDate();
+var day = (d < 10) ? '0' + d : d;
+var m = date.getMonth() + 1;
+var month = (m < 10) ? '0' + m : m;
+var yy = date.getYear();
+var year = (yy < 1000) ? yy + 1900 : yy;
+document.getElementById('date').innerHTML = day + '/' + month + '/' + year;
+//]]>
